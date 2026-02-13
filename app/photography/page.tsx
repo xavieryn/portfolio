eeb482b3e1d84bdd1ff7319bf86b358b76dc4f78
xavieryn <xavier.nishikawa@gmail.com>
2026-02-12 22:01:37 -0500

    Updates photography location

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -4,19 +4,19 @@ import Header from '@/components/Header'
 import { motion } from 'framer-motion'
 
 const photos = [
-  { id: 1, src: '/IMG_1270.jpg', alt: 'Photography portfolio image 1' },
-  { id: 2, src: '/IMG_1363.jpg', alt: 'Photography portfolio image 2' },
-  { id: 3, src: '/_DSF3362.jpg', alt: 'Photography portfolio image 3' },
-  { id: 4, src: '/_DSF3371.jpg', alt: 'Photography portfolio image 4' },
-  { id: 5, src: '/_DSF3399.JPG', alt: 'Photography portfolio image 5' },
-  { id: 6, src: '/_DSF3915.jpg', alt: 'Photography portfolio image 6' },
-  { id: 7, src: '/_DSF5010.jpg', alt: 'Photography portfolio image 7' },
-  { id: 8, src: '/_DSF5265.jpg', alt: 'Photography portfolio image 8' },
-  { id: 9, src: '/_DSF5330.jpg', alt: 'Photography portfolio image 9' },
-  { id: 10, src: '/_DSF6388.jpg', alt: 'Photography portfolio image 10' },
-  { id: 11, src: '/_DSF6603.jpg', alt: 'Photography portfolio image 11' },
-  { id: 12, src: '/_DSF7058.jpg', alt: 'Photography portfolio image 12' },
-  { id: 13, src: '/_DSF7060.jpg', alt: 'Photography portfolio image 13' },
+  { id: 1, src: '/photography/IMG_1270.jpg', alt: 'Photography portfolio image 1' },
+  { id: 2, src: '/photography/IMG_1363.jpg', alt: 'Photography portfolio image 2' },
+  { id: 3, src: '/photography/_DSF3362.jpg', alt: 'Photography portfolio image 3' },
+  { id: 4, src: '/photography/_DSF3371.jpg', alt: 'Photography portfolio image 4' },
+  { id: 5, src: '/photography/_DSF3399.JPG', alt: 'Photography portfolio image 5' },
+  { id: 6, src: '/photography/_DSF3915.jpg', alt: 'Photography portfolio image 6' },
+  { id: 7, src: '/photography/_DSF5010.jpg', alt: 'Photography portfolio image 7' },
+  { id: 8, src: '/photography/_DSF5265.jpg', alt: 'Photography portfolio image 8' },
+  { id: 9, src: '/photography/_DSF5330.jpg', alt: 'Photography portfolio image 9' },
+  { id: 10, src: '/photography/_DSF6388.jpg', alt: 'Photography portfolio image 10' },
+  { id: 11, src: '/photography/_DSF6603.jpg', alt: 'Photography portfolio image 11' },
+  { id: 12, src: '/photography/_DSF7058.jpg', alt: 'Photography portfolio image 12' },
+  { id: 13, src: '/photography/_DSF7060.jpg', alt: 'Photography portfolio image 13' },
 ]
 
 export default function Photography() {
